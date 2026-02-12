ee51a8f3648ebb0ea2cf9c5e828ade900f4eb79b
benperche <benperche2@gmail.com>
2026-02-12 18:47:52 +1100

    Initial Commit

---
+++ Music PDF Wrangler/Music_PDF_WranglerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_PDF_WranglerApp.swift
+//  Music PDF Wrangler
+//
+//  Created by Ben Perche on 12/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct Music_PDF_WranglerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
